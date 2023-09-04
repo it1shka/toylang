239fafd77a2911b0e16febda02345c586a7afdf3
it1shka <2tishbel@gmail.com>
2023-09-04 23:14:25 +0200

    Now, I created a good repl to test my parser

--- main.cpp
+++ main.cpp
@@ -3,22 +3,30 @@
 #include <sstream>
 #include "parser/parser.h"
 
-const std::string sample = R"(
-        fun sum(a, b) {
-            let output = 0.1120000;
-            for (_i from a to b) {
-                output += _i;
-            }
-            return output;
-        }
-    )";
+auto getInput() -> std::string {
+    std::string output, line;
+    while (true) {
+        std::cout << "> ";
+        std::getline(std::cin, line);
+        if (line == "exit") return "exit";
+        if (line == "STOP") break;
+        output += line + "\n";
+    }
+    return output;
+}
 
 int main() {
-    std::istringstream stream(sample);
-    auto parser = parser::Parser(stream);
-    auto ast = parser.buildAST();
-    for (const auto &each : parser.getErrors()) {
-        std::cout << each << std::endl;
+    while (true) {
+        auto input = getInput();
+        if (input == "exit") break;
+
+        std::istringstream stream(input);
+        auto parser = parser::Parser(stream);
+        auto ast = parser.buildAST();
+        std::cout << programToString(ast, 2) << std::endl;
+        for (const auto &each : parser.getErrors()) {
+            std::cout << each << std::endl;
+        }
+
     }
-    std::cout << parser::AST::programToString(ast, 4);
 }
